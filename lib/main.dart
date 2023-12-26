5a4d7cd5102d51ba3cda323951675df0837caaaa
Your Name <spectrumgithubtraining>
2023-12-26 11:11:30 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_module2_pgms_spectrum/Container/eg1.dart';
 import 'package:flutter_module2_pgms_spectrum/snackbar/eg1.dart';
 
 import 'List_view/eg1.dart';
+import 'Tab_bar/eg1.dart';
 import 'Text/eg1.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ListViewSample(),
+      home: TabBarSample(),
     );
   }
 }
